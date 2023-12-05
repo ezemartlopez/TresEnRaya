53ff04f12ff51ff5e6c22eba1d7edf8abefac363
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-12-05 19:03:23 -0300

    carga del proyecto tres en raya con react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Grilla from './Grilla/grilla';
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title-game'>Tres en Raya</h1>
+      <Grilla></Grilla>
+    </div>
+  );
+}
+
+export default App;
