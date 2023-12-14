26b56e406478ec8d3528b55066e692262636e9ae
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-12-14 15:40:20 -0300

    agregando a mi juego contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Grilla from './Grilla/grilla';
+import { ContextoGrillaJuego } from './Contexto/contextoGrilla';
 function App() {
   return (
     <div className="App">
-      <h1 className='title-game'>Tres en Raya</h1>
-      <Grilla></Grilla>
+      <h1 className='title-game'>Juego Tres en Raya</h1>
+      <ContextoGrillaJuego>
+        <Grilla></Grilla>
+      </ContextoGrillaJuego>
+      
     </div>
   );
 }
