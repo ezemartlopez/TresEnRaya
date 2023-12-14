3571578da8fa299ce54455695eedab109da9c5b7
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-12-14 15:42:11 -0300

    quitando la logia del componente y llamandola en el contexto

--- src/Grilla/grilla.jsx
+++ src/Grilla/grilla.jsx
@@ -1,79 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { modificarElementoEnLista, hayGanador, quitarBordes, todasCasillasMarcadas } from './funciones';
+import React from 'react';
+//import { modificarElementoEnLista, hayGanador, quitarBordes, todasCasillasMarcadas } from './funciones';
 import './grilla.css';
-import circulo from './images/circulo.png';
-import cruz from './images/cruz.png';
+import { GetContextoGrilla } from '../Contexto/contextoGrilla';
+import Card from './Card/CardMesagge';
+import ReactIf from './ReactIf/ReactIf';
 
-const CRUZ = 'cruz';
-const CIRCULO = 'circulo';
-const iconoAsignar = (valor) => {
-    if(valor === CRUZ) return cruz;
-    else return circulo;
-};
-function GrillaItem({valor, handleClick, index}){
-    const clickMe = ()=> { handleClick(index); }
+function GrillaItem({pos}){
+    const {modificarPosicionGrilla, itemUsado, getItemIcono} = GetContextoGrilla();
+    const clickItem = () => {
+        modificarPosicionGrilla(pos);
+    };
     return (
-        <div id={index} className="grid-item container-icon" onClick={clickMe}>
-            {valor && <img src={iconoAsignar(valor)} alt="imagen"/>}
+        <div id={pos} className="grid-item container-icon" onClick={clickItem}>
+            {itemUsado(pos) && <img src={getItemIcono(pos)} alt="imagen_icono" />}
         </div> 
     );
 }
 
-
 function Grilla(){
-    //Valores de la grilla del juego por default null.
-    const [board, setBoard] = useState(Array(9).fill(null));
-    //Turno de jugador, por default cruz
-    const [jugador, setJugador] = useState(CRUZ);
-    //estado para controlar el ganador
-    const [ganador, setGanador] = useState(false);
-    //estado de empate
-    const [empate, setEmpate] = useState(false);
-    //Control de cambios de Board
-    useEffect(()=>{
-        let resultado = hayGanador(board);
-        if(resultado){ setGanador(resultado); }
-        else if(todasCasillasMarcadas(board)){
-            setEmpate(true);
-        }
-    },[board]);
-    //funcion para alternar el jugador
-    const siguienteJugador = () => {
-        //Devuelve el jugador que debe marcar y cambia al sig
-        const auxiliarJugador = jugador;
-        if (jugador === CRUZ) setJugador(CIRCULO); 
-        else  setJugador(CRUZ);
-        return auxiliarJugador;
-    };
-    //Evento para manejar los clicks
-    const clickCasilla = (posicion) => {
-        if(board[posicion]===null && !ganador){
-            console.log('Click en pocision: ', posicion);
-            let cambioGrilla = modificarElementoEnLista(board,posicion, siguienteJugador());
-            setBoard(cambioGrilla);
-        }
-        return
-    };
-    //Evento que reinicia el tablero y estados
-    const reiniciar = () => {
-        setBoard(Array(9).fill(null));
-        setJugador(CRUZ);
-        setGanador(false);
-        setEmpate(false);
-        quitarBordes();
-    };
+    const {longitudGrilla, turnoJugador, tenemosUnGanador, tenemosUnEmpate, getGanador, reiniciarJuego} = GetContextoGrilla();
+    let items = [];
+    for (let index = 0; index < longitudGrilla(); index++) { items.push(<GrillaItem key={index} pos={index}/>); }
 
     return (
         <React.StrictMode>
-        <div className="container-game">
-            <div className="grid-container">
-                {board.map((valor, index) => ( <GrillaItem key={index} index={index} valor={valor} handleClick={clickCasilla} /> ))}
+            <div className="container-game">
+                <div className="grid-container">
+                    {items}
+                </div>
+                <div className="container-player">
+                    <p className='info-turn-play'>Turno Jugador</p>
+                    <div className="container-img">
+                       <img src={turnoJugador()} alt="jugador" className="customImg"/> 
+                    </div>
+                    <ReactIf state={tenemosUnGanador()}>
+                        <Card>
+                            <p className='info-winner'>EL GANADOR ES</p>
+                            <div className="container-winner">
+                               <img src={getGanador()} alt="icono_ganador" className='custom-img'/> 
+                            </div>
+                            <button className="w3-button w3-indigo w3-round-large" onClick={reiniciarJuego}>REINICIAR JUEGO</button>
+                        </Card>
+                    </ReactIf>
+                    <ReactIf state={tenemosUnEmpate()}>
+                        <Card>
+                            <p className='info-winner'>EMPATE !!</p>
+                            <button className="w3-button w3-indigo w3-round-large" onClick={reiniciarJuego}>REINICIAR JUEGO</button>
+                        </Card>
+                    </ReactIf>                   
+                </div>
+
             </div>
-            {(!ganador && !empate) && <p className='info-turn-play'> Turno para jugar: {jugador.toUpperCase()} </p>}
-            {ganador && <p className='info-winner'>El ganador es: {ganador.toUpperCase()}</p>}
-            {empate && <p className='info-winner'>EMPATE</p>}
-            {(ganador||empate) && <button className='btn' type='button' onClick={reiniciar}>REINICIAR</button>}
-        </div>
         </React.StrictMode>
     );
 }
